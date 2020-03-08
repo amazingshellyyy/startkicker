b20a5cade6275d46c07aafb70632a85fde713a20
amazingshellyyy <amazingshellyyy@gmail.com>
2020-03-07 22:20:55 -0800

    build change homepage

---
+++ docs/build/precache-manifest.1765f2a7a11ca1f8dc353d8abf6ccf30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1867255269b948dcbd5f877389550d7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "069c66327cc92ac55206",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "48f7e82d0d8783898cde",
+    "url": "./static/js/2.4024f285.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.4024f285.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "069c66327cc92ac55206",
+    "url": "./static/js/main.59d9001f.chunk.js"
+  },
+  {
+    "revision": "2e4071f51402e00bf9b3",
+    "url": "./static/js/runtime-main.590084d8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
