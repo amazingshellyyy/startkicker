a700ff4998efb044803b0e40d279b5738c583584
amazingshellyyy <amazingshellyyy@gmail.com>
2020-03-07 22:08:29 -0800

    build

---
+++ frontend/build/precache-manifest.45e43b2478a942d66ec597325b3c86e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "712f737a3dd3e6caf206e3bfa9e28673",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da4aaa82055669b6c330",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "48f7e82d0d8783898cde",
+    "url": "/static/js/2.4024f285.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.4024f285.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da4aaa82055669b6c330",
+    "url": "/static/js/main.4235a0dd.chunk.js"
+  },
+  {
+    "revision": "7a008b5c3a6c3fefae51",
+    "url": "/static/js/runtime-main.18445cb3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
