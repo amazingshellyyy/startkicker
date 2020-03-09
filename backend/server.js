6d7730c39935a41a645adb298af65398464b6583
amazingshellyyy <amazingshellyyy@gmail.com>
2020-03-08 22:44:11 -0700

    sets up cors

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const morgan = require('morgan')
 const db = require('./models');
+const cors = require('cors');
 const app = express();
 
 //env setup
@@ -9,6 +11,13 @@ const PORT = process.env.PORT;
 // const routes = require('./routes');
 
 //-----Middleware---//
+const coresOptions = {
+  origin: ['http://localhost:3000', 'http://amazingshellyyy.com','https://amazingshellyyy.com', 'http://amazingshelly.github.com'],
+  optionsSuccessStatus: 200
+}
+
+
+app.use(cors(coresOptions))
 app.use(bodyParser.json());
 
 //-----Routes----//
