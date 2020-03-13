0118a79c76854769352d8df5dffdd1a7f6749ee2
amazingshellyyy <amazingshellyyy@gmail.com>
2020-03-13 03:16:02 -0700

    commit for update before deploy

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 
 ReactDOM.render(
-  <Router>
+  <HashRouter>
 <App />
-</Router>, document.getElementById('root'));
+</HashRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
