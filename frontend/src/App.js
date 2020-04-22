895f86493eef89c086f94083f15c5b077d6daa4a
amazingshellyyy <amazingshellyyy@gmail.com>
2020-04-22 13:48:10 -0700

    commit for update before deploy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import './App.css';
-
 import Naviga from './components/Navbar/Navbar';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Routes from './config/routes';
 import { withRouter } from "react-router";
 import jwt from 'jsonwebtoken';
 import Footer from './components/Footer/Footer'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 class App extends Component {
   state = {
