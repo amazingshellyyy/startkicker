8e1bf2a5966c32085ebdee2e96ddb0fa2ffd072a
amazingshellyyy <amazingshellyyy@gmail.com>
2020-04-23 21:22:45 -0700

    commit for update before deploy

--- docs/precache-manifest.7f4e2608d895388afb947c3184b11980.js
+++ docs/precache-manifest.7f4e2608d895388afb947c3184b11980.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "057f4338e9446b1f8d5e6ad027f9a4ac",
+    "revision": "011230027276a152ea165d47991a0d6a",
     "url": "./index.html"
   },
   {
-    "revision": "0a48ef7bf971239b3d3b",
+    "revision": "9d4b42370ca5ad7e049d",
     "url": "./static/css/2.8a357d2f.chunk.css"
   },
   {
-    "revision": "90c99b6188f7876b7894",
+    "revision": "8835a3c332d8a3245d25",
     "url": "./static/css/main.71cbd4cd.chunk.css"
   },
   {
-    "revision": "0a48ef7bf971239b3d3b",
-    "url": "./static/js/2.6acc74f6.chunk.js"
+    "revision": "9d4b42370ca5ad7e049d",
+    "url": "./static/js/2.515fbfeb.chunk.js"
   },
   {
     "revision": "b9f28c176efdd716457c02939f4eac67",
-    "url": "./static/js/2.6acc74f6.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.515fbfeb.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "90c99b6188f7876b7894",
-    "url": "./static/js/main.734a0413.chunk.js"
+    "revision": "8835a3c332d8a3245d25",
+    "url": "./static/js/main.9134d59c.chunk.js"
   },
   {
     "revision": "2e4071f51402e00bf9b3",
