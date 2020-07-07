8a88746838e48a01f8a5a5396647dad6e20dafe7
amazingshellyyy <amazingshellyyy@gmail.com>
2020-07-07 09:24:27 -0700

    commit for update before deploy

--- backend/server.js
+++ backend/server.js
@@ -13,20 +13,9 @@ const stripe = require('stripe')(`${process.env.STRIPE_KEY}`);
 
 
 //-----Middleware---//
-const originList = ['http://localhost:3000','https://amazingshellyyy.com']
-const coresOptions = {
-  origin: function(origin, callback) {
-      if (originList.indexOf(origin) !== -1) {
-          callback(null, true)
-      } else {
-          callback(new Error('Not allow by cors'))
-      }
-  },
-  credentials: true,
-  optionsSuccessStatus: 200
-}
 
-app.use(cors(coresOptions));
+
+app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
